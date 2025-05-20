da34fad138285804f3f80945401ba115c9a1e411
rhawan1 <rhawanaugusto13@gmail.com>
2025-05-20 10:31:17 -0300

    Melhorando as automações

--- stacks/zabbix/mysql/init/init.sql
+++ stacks/zabbix/mysql/init/init.sql
@@ -1,3 +1,11 @@
-CREATE USER 'zbx_monitor'@'%' IDENTIFIED BY 'zbx4monitor';
+-- Usuário geral para monitoramento MySQL
+CREATE USER IF NOT EXISTS 'zbx_monitor'@'%' IDENTIFIED BY 'zbx4monitor';
 GRANT REPLICATION CLIENT,PROCESS,SHOW DATABASES,SHOW VIEW ON *.* TO 'zbx_monitor'@'%';
+FLUSH PRIVILEGES;
+
+-- Banco do GLPi
+CREATE DATABASE IF NOT EXISTS glpi_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE USER IF NOT EXISTS 'glpi_user'@'%' IDENTIFIED BY 'glpi_password';
+GRANT ALL PRIVILEGES ON glpi_db.* TO 'glpi_user'@'%';
+
 FLUSH PRIVILEGES;
